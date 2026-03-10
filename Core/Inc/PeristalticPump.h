d757ef5b2a392f672e2302caf39e1750631d4927
RainerD <rainer.ds03@gmail.com>
2026-03-10 14:07:45 -0700

    Tested Peristaltic pump code and made minor modifications to .c file

--- Core/Inc/PeristalticPump.h
+++ Core/Inc/PeristalticPump.h
@@ -3,6 +3,15 @@
  *
  *  Created on: Mar 5, 2026
  *      Author: Rainer
+ *
+ *      Notes:
+ *      pump V+ ---> Power rail
+ *      pump GND ---> MOSFET drain
+ *      Pump V+ ---> DIODE Stripe side --> MOS Drain
+ *      MOS Gate --> Resistor --> GPIO output
+ *      MOS Source ---> GND MCU
+ *      12V external V+ --> power rail
+ *      12V GND --> GND rail
  */
 
 #ifndef INC_PERISTALTICPUMP_H_
@@ -13,15 +22,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include "stm32l4xx_hal.h"
+
 
 // Helps individualize each pump
 typedef struct{
-	char name[50];
+	char name[100];
 	GPIO_TypeDef* GPIOx;
 	uint16_t GPIO_Pin;
 }Pump;
 
 Pump pump_init(char* Name,GPIO_TypeDef* GPIO, uint16_t Pin);
+void runPump(Pump pump,int time);
 void runPump_1(Pump pump);
 void runPump_10(Pump pump);
 
